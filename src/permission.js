a6f8ed33576338833f2f1bbc7b93a2100cf91489
279467439 <279467439@qq.com>
2018-03-28 22:56:27 +0800

    update

---
+++ src/permission.js
@@ -0,0 +1,103 @@
+import router from '@/router'
+import store from './store'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css'// progress bar style
+import { getToken } from '@/utils/auth'
+import { setTitle } from '@/utils';
+import { validatenull } from '@/utils/validate';
+import { asyncRouterMap } from '@/router'
+NProgress.configure({ showSpinner: false })// NProgress Configuration
+function hasPermission(roles, permissionRoles) {
+    if (!permissionRoles) return true
+    return roles.some(role => permissionRoles.indexOf(role) >= 0)
+}
+//定义白名单(路径不需要tag打开)
+const whiteList = ['/login', '/404', '/401', '/lock']
+const lockPage = '/lock'
+
+// 动态添加可访问路由表
+router.addRoutes(asyncRouterMap); 
+
+//路由进入前拦截
+router.beforeEach((to, from, next) => {
+    //路由进入前开始进度条
+    NProgress.start() // start progress bar
+    const value = to.query.src ? to.query.src : to.path;
+    const label = to.query.name ? to.query.name : to.name;
+    //判断路径是否属于白名单
+    console.log("11111111111")
+    if (whiteList.indexOf(value) == -1) {
+        store.commit('ADD_TAG', {
+            label: label,
+            value: value,
+            query: to.query
+        });
+    }
+    //检测是否存在token
+    if (getToken()) {
+        //存在token 放行
+        next();
+    } else {
+        //不存在token
+        if (whiteList.indexOf(to.path) !== -1) {
+            //白名单放行
+            next()
+        } else {
+            //非白名单 跳转到登录页
+            next('/login')
+            NProgress.done();
+        }
+    }
+    
+})
+
+//寻找子菜单的父类
+function findMenuParent(tag) {
+    let tagCurrent = [];
+    const menu = store.getters.menu;
+    tagCurrent.push(tag);
+    return tagCurrent;
+    // //如果是一级菜单直接返回
+    // for (let i = 0, j = menu.length; i < j; i++) {
+    //     if (menu[i].href == tag.value) {
+    //         tagCurrent.push(tag);
+    //         return tagCurrent;
+    //     }
+    // }
+
+    // let currentPathObj = menu.filter(item => {
+    //     if (item.children.length == 1) {
+    //         return item.children[0].href === tag.value;
+    //     } else {
+    //         let i = 0;
+    //         let childArr = item.children;
+    //         let len = childArr.length;
+    //         while (i < len) {
+    //             if (childArr[i].href === tag.value) {
+    //                 return true;
+    //                 break;
+    //             }
+    //             i++;
+    //         }
+    //         return false;
+    //     }
+    // })[0];
+    // tagCurrent.push({
+    //     label: currentPathObj.label,
+    //     value: currentPathObj.href
+    // });
+    // tagCurrent.push(tag);
+    // return tagCurrent;
+
+}
+
+//路由进入后
+router.afterEach((to, from) => {
+    //关闭进度条
+    NProgress.done();
+    setTimeout(() => {
+        const tag = store.getters.tag;
+        setTitle(tag.label);
+        store.commit('SET_TAG_CURRENT', findMenuParent(tag));
+    }, 0);
+})
---
+++ src/views/exhibition/index.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="krt-wrapper">
+    <div class="krt-search">
+        <el-form :inline="true" :model="dataForm" class="krt-search-form" size="small">
+        <el-form-item label="审批人">
+          <el-input  placeholder="审批人" v-model="dataForm.param1"></el-input>
+        </el-form-item>
+        <el-form-item label="审批人">
+          <el-input  placeholder="审批人" v-model="dataForm.param2"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary"  @click="getDataList()">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+  <div class="krt-box">
+  <div class="krt-box-body">  
+    <div class="krt-do-button">
+        <el-button  type="primary" size="small" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button  type="danger" size="small" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+    </div>
+    <el-table
+      :data="dataList" 
+      ref="multipleTable" 
+      stripe
+      border
+      tooltip-effect="dark"
+      @selection-change="selectionChangeHandle">
+      <el-table-column 
+        type="selection"
+        prop="id" 
+        width="55">
+      </el-table-column>
+      <el-table-column 
+        prop="username" 
+        header-align="center" 
+        align="center" 
+        label="用户账号">
+      </el-table-column>
+      <el-table-column 
+        prop="name" 
+        header-align="center" 
+        align="center" 
+        label="用户姓名">
+      </el-table-column>
+      <el-table-column 
+        prop="roleName" 
+        header-align="center" 
+        align="center" 
+        label="角色名">
+      </el-table-column>
+      <el-table-column 
+        prop="organizationName" 
+        header-align="center" 
+        align="center" 
+        label="所属机构">
+      </el-table-column>
+      <el-table-column 
+        prop="status" 
+        header-align="center" 
+        align="center" 
+        label="账号状态">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.status === 1" size="small" type="danger">禁用</el-tag>
+          <el-tag v-else size="small">正常</el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+           <el-button  type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="krt-page"> 
+     <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    </div>  
+   </div>
+  </div>
+  <!-- 弹窗, 新增 / 修改 -->
+  <iu v-if="addOrUpdateVisible" ref="iu"  @refreshDataList="getDataList"></iu>
+</div>
+</template>
+
+<script>
+import iu from "./iu";
+export default {
+  name: 'user',
+  data() {
+    return {
+      dataForm: {
+        param1: "",
+        param2: ""
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    };
+  },
+  components: {
+    iu
+  },
+  activated() {
+    this.getDataList();
+  },
+  created(){
+    this.getDataList();
+  },
+  methods: {
+    // 获取数据列表
+    getDataList() {
+      this.dataListLoading = true;
+      var params = {
+        page: this.pageIndex,
+        limit: this.pageSize,
+        param1: this.dataForm.param1
+      };
+      this.$API.user.list(params).then(({ data }) => {
+        if (data && data.code === 0) {
+          console.log(data);
+          this.dataList = data.page.list;
+          this.totalPage = data.page.totalCount;
+        } else {
+          this.dataList = [];
+          this.totalPage = 0;
+        }
+        this.dataListLoading = false;
+      });
+    },
+    // 每页数
+    sizeChangeHandle(val) {
+      this.pageSize = val;
+      this.pageIndex = 1;
+      this.getDataList();
+    },
+    // 当前页
+    currentChangeHandle(val) {
+      this.pageIndex = val;
+      this.getDataList();
+    },
+    // 多选
+    selectionChangeHandle(val) {
+      this.dataListSelections = val;
+    },
+    // 新增 / 修改
+    addOrUpdateHandle(id) {
+      this.addOrUpdateVisible = true;
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(id);
+      });
+    },
+    // 删除
+    deleteHandle(id) {
+      var userIds = id
+        ? [id]
+        : this.dataListSelections.map(item => {
+            return item.id;
+          });
+      this.$confirm(
+        `确定对[id=${userIds.join(",")}]进行[${id ? "删除" : "批量删除"}]操作?`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }
+      ).then(() => {
+        API.user.del(userIds).then(({ data }) => {
+          if (data && data.code === 0) {
+            this.$message({
+              message: "操作成功",
+              type: "success",
+              duration: 1500,
+              onClose: () => {
+                this.getDataList();
+              }
+            });
+          } else {
+            this.$message.error(data.msg);
+          }
+        });
+      });
+    }
+  }
+};
+</script>
